52041454575d640eaf03760de2503a8a97c37acf
Pusha <de24world@gmail.com>
2021-02-13 21:42:45 +0100

    add popup.scss and disabledstyles

--- src/Popup.js
+++ src/Popup.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Popup.scss";
 // Basic
 // import CookieConsent from "react-cookie-consent";
 
@@ -15,15 +16,15 @@ const Popup = () => {
 
       {/* option */}
       <CookieConsent
-        location="bottom"
+        disableStyles
+        location="none"
         buttonText="Sure man!!"
         cookieName="myAwesomeCookieName2"
-        style={{ background: "#2B373B" }}
-        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-        expires={150}
+        // style={{ background: "#2B373B" }}
+        // buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        // expires={150}
       >
         This website uses cookies to enhance the user experience.{" "}
-        <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
       </CookieConsent>
     </div>
   );
