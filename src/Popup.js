66c47e97c7b6e47819c5b5f22dc750d25a55bd52
Pusha <de24world@gmail.com>
2021-02-15 00:16:11 +0100

    update overlay

--- src/Popup.js
+++ src/Popup.js
@@ -8,7 +8,7 @@ import CookieConsent, { Cookies } from "react-cookie-consent";
 
 const Popup = () => {
   return (
-    <div>
+    <div className="popup">
       {/* Basic */}
       {/* <CookieConsent>
         This website uses cookies to enhance the user experience.
@@ -18,13 +18,23 @@ const Popup = () => {
       <CookieConsent
         disableStyles
         location="none"
-        buttonText="Sure man!!"
+        buttonText="Akzeptieren"
         cookieName="myAwesomeCookieName2"
+        overlay
+        overlayClasses="overlayclass"
         // style={{ background: "#2B373B" }}
         // buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
         // expires={150}
       >
-        This website uses cookies to enhance the user experience.{" "}
+        Wir nutzen auf unseren Webseiten Cookies und Trackingtechnologien. Diese
+        dienen der Optimierung unserer Website, der Weiterentwicklung von
+        Services und Marketingzwecken. Der Einsatz bestimmter Cookies ist für
+        die uneingeschränkte Nutzung unserer Website technisch erforderlich.
+        Durch Klick auf „Akzeptieren“ stimmen Sie zu, dass auch Cookies zu
+        Analyse-, Marketing- und Social Media-Zwecken gesetzt werden. Die
+        Einwilligung können Sie jederzeit widerrufen. Weitere Informationen
+        sowie die Widerspruchsmöglichkeit finden Sie in unserer
+        Datenschutzinformation .<button>Einstellung</button>
       </CookieConsent>
     </div>
   );
